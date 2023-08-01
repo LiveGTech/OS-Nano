a0a7ef050d0257e8de540fc08b0d547dcd3c0e4d
James-Livesey <jamesliveg@gmail.com>
2023-08-01 22:06:45 +0200

    Add emsdk and EpoxyDuino as submodules

--- installdev.sh
+++ installdev.sh
@@ -5,14 +5,7 @@
 # https://liveg.tech/os
 # Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 
-rm -rf installdev
-mkdir -p installdev
-
 pushd installdev
-    git clone https://github.com/juj/emsdk.git
-
     emsdk/emsdk install latest
     emsdk/emsdk activate latest
-
-    git clone https://github.com/bxparks/EpoxyDuino
 popd
