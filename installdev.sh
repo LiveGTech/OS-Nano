e4404437626858f51f6d8dc2e1331ca6e9afb197
James-Livesey <jamesliveg@gmail.com>
2023-08-01 22:11:53 +0200

    Add Duktape for future inclusion

--- installdev.sh
+++ installdev.sh
@@ -6,6 +6,12 @@
 # Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 
 pushd installdev
+    rm -rf duktape
+
+    wget https://duktape.org/duktape-2.7.0.tar.xz -O duktape.tar.xz
+    tar -xf duktape.tar.xz
+    mv duktape-2.7.0 duktape
+
     emsdk/emsdk install latest
     emsdk/emsdk activate latest
 popd
