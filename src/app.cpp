d0ae59ac46b9fd25a45d7ed857157b8294fa51d8
James-Livesey <jamesliveg@gmail.com>
2023-08-03 23:33:54 +0200

    Add app JS execution

--- src/app.cpp
+++ src/app.cpp
@@ -8,30 +8,67 @@
 */
 
 #include <Arduino.h>
+#include <duktape.h>
 
 #include "app.h"
 #include "proc.h"
 
 struct ProcessTaskState {
     String id;
+    duk_context* duktapeContextPtr;
 };
 
+proc::Process* getProcessFromDuktapeContext(duk_context* ctx) {
+    duk_memory_functions functions;
+
+    duk_get_memory_functions(ctx, &functions);
+
+    return (proc::Process*)functions.udata;
+}
+
 void processTask(proc::Process* processPtr) {
-    Serial.println("Hello, world, from an app!");
+    auto state = (ProcessTaskState*)processPtr->taskState;
+    auto ctx = state->duktapeContextPtr;
+
+    duk_eval_string_noresult(ctx, "print('Hello from an app!');");
 
     processPtr->stopAndDiscard();
 }
 
 void processCleanupHandler(proc::Process* processPtr) {
-    Serial.println("App cleanup");
+    auto state = (ProcessTaskState*)processPtr->taskState;
+
+    duk_destroy_heap(state->duktapeContextPtr);
+}
+
+duk_ret_t native_print(duk_context* ctx) {
+    auto processPtr = getProcessFromDuktapeContext(ctx);
+    auto state = (ProcessTaskState*)processPtr->taskState;
+
+    duk_push_string(ctx, " ");
+    duk_insert(ctx, 0);
+    duk_join(ctx, duk_get_top(ctx) - 1);
+
+    Serial.print("[");
+    Serial.print(state->id);
+    Serial.print("] ");
+    Serial.println(duk_safe_to_string(ctx, -1));
+
+    return 0;
 }
 
 void app::launch(String id) {
     auto processTaskState = new ProcessTaskState();
 
-    processTaskState->id = id;
-
     auto process = new proc::Process(processTask, processTaskState);
+
+    auto ctx = duk_create_heap(NULL, NULL, NULL, process, NULL);
+
+    processTaskState->id = id;
+    processTaskState->duktapeContextPtr = ctx;
+
+    duk_push_c_function(ctx, native_print, DUK_VARARGS);
+    duk_put_global_string(ctx, "print");
 
     process->setCleanupHandler(processCleanupHandler);
 }
