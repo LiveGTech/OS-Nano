16ffc13589cce622724052c3fa0b4a6843ef5bd4
y-honey <y.honey0128@gmail.com>
2024-02-08 13:03:14 -0600

    change of python3 command to get platformio.py

--- installdev.sh
+++ installdev.sh
@@ -9,7 +9,9 @@
 
 npm install -g terser
 
-python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py)"
+sudo apt-get install python3.10-venv
+curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
+python3 get-platformio.py
 
 pushd installdev
     rm -rf duktape
